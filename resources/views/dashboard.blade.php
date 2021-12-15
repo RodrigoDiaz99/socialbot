e6f6bde4753f91b222d24ab1c67e94aa5a64b2ef
Rodrigo Diaz <diaz-rodrigo@hotmail.com>
2021-12-14 23:57:27 -0600

    update bootstrap 5

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,6 +4,9 @@
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
-
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Launch demo modal
+      </button>
+      @include('modal')
     <x-jet-welcome />
 </x-app-layout>
