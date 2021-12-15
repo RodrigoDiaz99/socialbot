84cd3cea2802b8a7362477ed3f30c69c6949e78f
Rodrigo Diaz <diaz-rodrigo@hotmail.com>
2021-12-14 22:42:13 -0600

    creat model, migrations and controller

---
+++ app/Http/Controllers/TwitterController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\twitter;
+use Illuminate\Http\Request;
+
+class TwitterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\twitter  $twitter
+     * @return \Illuminate\Http\Response
+     */
+    public function show(twitter $twitter)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\twitter  $twitter
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(twitter $twitter)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\twitter  $twitter
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, twitter $twitter)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\twitter  $twitter
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(twitter $twitter)
+    {
+        //
+    }
+}
